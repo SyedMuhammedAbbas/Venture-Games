3220722c07cc27df4082f4b67bff5dbb6a4237b3
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-08-29 17:17:24 +0500

    Updated

--- components/ClientsView.js
+++ components/ClientsView.js
@@ -11,8 +11,8 @@ export default function ClientsView() {
                 </div>
 
                 <div className="flex m-[50px] xl:m-[30px] xl2:m-[10vw] lg:m-10  gap-20 xl:gap-10 xl1:gap-5 xl2:gap-10 2xl:gap-14 justify-center pb-[50px] xl2:grid xl2:grid-cols-2 tablet3:grid-cols-1 tablet3:ml-[25vw] tablet2.1:ml-[20vw] mobile:ml-[17vw] mobile2.1:ml-[10vw] mobile1:ml-[5vw] mobile1.1:m-auto tablet3:p-auto tablet3:">
-                {[...Array(n)].map(() =>
-                    <div className="bg-gradient-to-r from-black to-[#1c1c1c] rounded-2xl w-[340px] pt-10 mobile1.1:m-auto mobile1.1:p-auto mobile1.1:w-[300px]">
+                {[...Array(n)].map((user) =>
+                    <div key={user} className="bg-gradient-to-r from-black to-[#1c1c1c] rounded-2xl w-[340px] pt-10 mobile1.1:m-auto mobile1.1:p-auto mobile1.1:w-[300px]">
                         <img className="flex justify-center m-auto w-44 object-cover bg-slate-500 rounded-full mb-10" src={Profile.src} />
                         <div className="text-white  text-center mb-[15px]">Experience The Rise Of Miles Morales As The New Hero Masters Incredible, Explosive New Powers To Become His Own Spider- <br />Man</div>
                         <div className="flex justify-center m-auto border-t-8 w-[170px] border-[#FFB636] center pb-5"></div>
