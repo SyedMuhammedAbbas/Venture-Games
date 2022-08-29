957386ce43c3000160cb76d0ce364f24c4515f01
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-08-29 19:33:53 +0500

    Updated

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,6 @@ import TopGames from "../components/TopGames";
 export default function Home() {
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
-      />
-
-      <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
       <Header />
       <ProfileBar />
       <TopGames />
