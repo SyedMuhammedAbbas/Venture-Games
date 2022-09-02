f7e10841d79f28de4d9191d92421ee91862aeae6
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-09-02 16:15:03 +0500

    Files Updated

--- components/ShopCover.js
+++ components/ShopCover.js
@@ -2,9 +2,9 @@ import ShopBack from '../images/shop-background.png';
 
 export default function ShopCover(){
     return(
-        <div className='bg-black h-[90vh]'>
-            <img className='justify-center' src={ShopBack.src}></img>
-            <div className='text-white text-[3vw] text-center mt-[-120px] font-hemi'>All Your Gamming Needs</div>
+        <div className='grid bg-black h-[100vh] 5.1xl:pt-[7%] xl1:pt-[15%] tablet3:pt-[25%] mobile:pt-[35%] mobile2.1:pt-[45%] mobile1:pt-[65%] xl1:gap-[10%] tablet3:gap-[20%] mobile:gap-[50%]'>
+            <img className='justify-center object-contain' src={ShopBack.src}></img>
+            <div className='text-white text-[3vw] tablet3:text-[4.5vw] text-center mt-[-120px] lg:mb-[0px] font-hemi'>All Your Gamming Needs</div>
         </div>
     )
 }
