60249c69350f30d38a0514d6406f485b19fcff0c
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-09-10 03:28:10 +0500

    Files Updated

--- components/PlatformsElement.js
+++ components/PlatformsElement.js
@@ -17,10 +17,10 @@ export default function PlatformsElement() {
           Shop By Platform
         </div>
         <div className="flex gap-10 mobile:gap-5 xl:grid xl:grid-cols-2 mobile:grid-cols-2">
-          {platformLogos.map((platformLogos, index) => (
-            <Link href={`/${platformLogos}`}>
+          {platformLogos.map((index, platformLogos) => (
+            <Link key={index} href={`/${platformLogos}`}>
               <a>
-                <div key={index} className={platformClass}>
+                <div className={platformClass}>
                   <img className={platformImgClass} src={platformLogos.src} />
                 </div>
               </a>
