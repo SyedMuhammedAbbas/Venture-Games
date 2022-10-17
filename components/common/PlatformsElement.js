0eee015c6ea86c91909348e3026e5c183c397e46
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-10-17 18:05:28 +0500

    Updated

--- components/common/PlatformsElement.js
+++ components/common/PlatformsElement.js
@@ -3,32 +3,61 @@ import PS4 from "../../images/ps4-logo.png";
 import xbox from "../../images/xbox-logo.png";
 import nintendo from "../../images/nintendo.png";
 import Link from "next/link";
+import axios from "axios";
+import { useSelect } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-export default function PlatformsElement({ consoles, platforms }) {
+export default function PlatformsElement({ consoles }) {
+  const router = useRouter();
+  const [platforms, setPlatforms] = useState();
+  async function getPlatforms() {
+    let response = await axios.get("https://api.venturegames.pk/GetPlatforms");
+    setPlatforms(response.data);
+  }
+
+  async function handleClick(selectedPlatform) {
+    console.log(selectedPlatform._id);
+    router.push({
+      pathname: "/platforms",
+      query: { selectedPlatform: selectedPlatform._id },
+    });
+  }
+
+  useEffect(() => {
+    getPlatforms();
+  });
   const platformLogos = [PS5, xbox, PS4, nintendo];
   const platformClass =
     "snap-center border-[1px] w-[340px] h-[450px] flex justify-center items-center rounded-[35px]  xl1:rounded-[30px] border-white mobile:rounded-[35px]";
   const platformImgClass = " w-[290px]  justify-center";
   return (
     <div className="platformElement bg-gradient-to-tr from-[#805712] via-black to-[#805712] p-20 5xl:px-20 px-64 5xl:px-auto 4xl:px-10 2xl:px-auto mobile:p-4 min-h-[140vh] mobile:min-h-[100vh] max-h-[100%] flex justify-center mobile1:items-center">
-      <div className="grid gap-10 justify-center w-[125vw] platformElementMainClass border-[1px] mobile1:h-auto  border-white rounded-[40px]  p-10 tablet2:p-5 pt-0 my-[30px]">
-        <div className="text-[90px] 2xl:text-[70px] lg:text-[50px] tablet:text-[40px] mobile:text-[25px] text-center text-white font-hemi">
-          Shop By Platform
-        </div>
-        <div className="flex justify-center 2xl:justify-start gap-10 mobile:gap-5 w-[110vw] platformElementClass overflow-x-auto platform-items-scroll h-[500px] snap-x snap-mandatory">
-          {platformLogos.map((platformLogos, index) => (
-            <div key={index}>
-              <Link href="/shop">
-                <a>
+      {platforms && (
+        <div className="grid gap-10 justify-center w-[125vw] platformElementMainClass border-[1px] mobile1:h-auto  border-white rounded-[40px]  p-10 tablet2:p-5 pt-0 my-[30px]">
+          <div className="text-[90px] 2xl:text-[70px] lg:text-[50px] tablet:text-[40px] mobile:text-[25px] text-center text-white font-hemi">
+            Shop By Platform
+          </div>
+          <div className="flex justify-center 2xl:justify-start gap-10 mobile:gap-5 w-[110vw] platformElementClass overflow-x-auto platform-items-scroll h-[500px] snap-x snap-mandatory">
+            {platforms.map((platformLogos, index) => (
+              <div key={index}>
+                {/* <Link href="/shop"> */}
+                <a
+                  onClick={() => {
+                    handleClick(platformLogos);
+                  }}
+                >
                   <div className={platformClass}>
-                    <img className={platformImgClass} src={platformLogos.src} />
+                    {/* <img className={platformImgClass} src={platformLogos.src} /> */}
+                    {platformLogos.Title}
                   </div>
                 </a>
-              </Link>
-            </div>
-          ))}
+                {/* </Link> */}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
