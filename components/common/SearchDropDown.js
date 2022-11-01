42bfe99aacc420680b545019e113953bb189d104
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-11-01 14:01:23 +0500

    Updated

--- components/common/SearchDropDown.js
+++ components/common/SearchDropDown.js
@@ -8,14 +8,16 @@ export default function SearchDropDown() {
   return (
     <>
       <div className="bg-black absolute right-[12%] top-[100%] rounded-xl z-[9999] grid gap-2 w-[400px]">
-        {Object.values(searchResult).map((i) => {
-          return (
-            <div className="flex gap-5 bg-transparent border-b-[1px] p-5 border-blackOpac items-center ">
-              <img className="w-20 h-20 bg-contain" src={i.images}></img>
-              <div className="text-white text-[25px]">{i.title}</div>
-            </div>
-          );
-        })}
+        {Object.values(searchResult)
+          .slice(0, 5)
+          .map((i) => {
+            return (
+              <div className="flex gap-5 bg-transparent border-b-[1px] p-5 border-blackOpac items-center ">
+                <img className="w-20 h-20 bg-contain" src={i.images}></img>
+                <div className="text-white text-[25px]">{i.title}</div>
+              </div>
+            );
+          })}
         <button className="text-white text-[25px] p-2 hover:text-[#FFB636] text-center">
           View all
         </button>
--- components/common/SearchDropDownMobile.js
+++ components/common/SearchDropDownMobile.js
@@ -7,15 +7,17 @@ export default function SearchDropDown() {
   ];
   return (
     <>
-      <div className="bg-black fixed rounded-xl z-[9999] grid gap-2 w-[500px] mobile:w-[100%]">
-        {Object.values(searchResult).map((i) => {
-          return (
-            <div className="flex gap-5 bg-transparent border-b-[1px] p-5 border-blackOpac items-center ">
-              <img className="w-20 h-20 bg-contain" src={i.images}></img>
-              <div className="text-white text-[25px]">{i.title}</div>
-            </div>
-          );
-        })}
+      <div className="bg-black fixed rounded-xl z-[99999] grid gap-2 w-[500px] mobile:w-[100%]">
+        {Object.values(searchResult)
+          .slice(0, 5)
+          .map((i) => {
+            return (
+              <div className="flex gap-5 bg-transparent border-b-[1px] p-5 border-blackOpac items-center ">
+                <img className="w-20 h-20 bg-contain" src={i.images}></img>
+                <div className="text-white text-[25px]">{i.title}</div>
+              </div>
+            );
+          })}
         <button className="text-white text-[25px] p-2 hover:text-[#FFB636] text-center">
           View all
         </button>
