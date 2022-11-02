096cacae4e66c2144594256caa39f9560f9edaad
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-11-02 21:05:43 +0500

    Updated

--- pages/help/index.js
+++ pages/help/index.js
@@ -16,21 +16,21 @@ export default function Help() {
           sethandleSignup={sethandleSignup}
         />
       </div>
-      <div className="bg-[#FFB636] text-[20px] text-black pt-[10%] px-20 pb-20 min-h-[140vh] mobile:min-h-[100vh] max-h-[100%] tablet:pt-[20%] tablet:px-5 snap-start">
+      <div className="bg-[#FFB636] text-[20px] text-black pt-[10%] grid justify-center items-center px-20 pb-20 min-h-[140vh] mobile:min-h-[125vh] max-h-[100%] tablet:pt-[10%] tablet:px-5 snap-start">
         <div>
-          <h1 className="text-[65px] font-bold flex justify-center">
+          <h1 className="text-[65px] mobile:text-[30px] font-bold flex justify-center">
             How can we help you ?
           </h1>
           <br />
           <br />
           <ul className="flex flex-wrap justify-center gap-10">
             <Link href="/help/technicalIssues">
-              <li className="border border-black text-[#000] rounded-[10px] text-[30px] px-5 py-3 cursor-pointer">
+              <li className="border border-black text-[#000] rounded-[10px] text-[30px] mobile:text-[25px] mobile1:w-[250px] flex justify-center h-[80px] items-center px-5 py-3 cursor-pointer">
                 <span>Technical Issues </span>
               </li>
             </Link>
             <Link href="/help/generalQuestions">
-              <li className="border border-black text-[#000] rounded-[10px] text-[30px] px-5 py-3 cursor-pointer">
+              <li className="border border-black text-[#000] rounded-[10px] text-[30px] mobile:text-[25px] mobile1:w-[250px] flex justify-center h-[80px] items-center px-5 py-3 cursor-pointer">
                 <span>General Questions</span>
               </li>
             </Link>
--- pages/policies/index.js
+++ pages/policies/index.js
@@ -16,43 +16,44 @@ export default function Policies() {
           sethandleSignup={sethandleSignup}
         />
       </div>
-      <div className="bg-[#FFB636] snap-start text-[20px] min-h-[140vh] mobile:min-h-[100vh] max-h-[100%] text-black pt-[10%] px-20 pb-20 tablet:pt-[20%] tablet:px-5">
-        <h1 className="text-[45px]">
-          <span className="underline ">Policies</span>
-        </h1>
-        <br></br>
-        <ul className="flex flex-wrap justify-center gap-10">
-          <Link href="/policies/privacy">
-            <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
-              <span>Privacy policy</span>
-            </li>
-          </Link>
-          <Link href="/policies/shipping">
-            <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
-              <span>Shipping policy</span>
-            </li>
-          </Link>
-          <Link href="/policies/payment">
-            <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
-              <span>Payment Policy</span>
-            </li>
-          </Link>
-          <Link href="/policies/repairing">
-            <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
-              <span>Repairing policy</span>
-            </li>
-          </Link>
-          <Link href="/policies/return">
-            <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
-              <span>Return policy</span>
-            </li>
-          </Link>
-          <Link href="/policies/cancellation">
-            <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
-              <span>Cancellation policy</span>
-            </li>
-          </Link>
-        </ul>
+      <div className="bg-[#FFB636] snap-start text-[20px] min-h-[140vh] mobile:min-h-[125vh] max-h-[100%] grid justify-center text-black pt-[10%] px-20 pb-20 tablet:pt-[20%] tablet:px-5">
+        <div className="text-[65px] flex justify-center">
+          <span className="">Policies</span>
+        </div>
+        <div>
+          <ul className="flex flex-wrap justify-center gap-10">
+            <Link href="/policies/privacy">
+              <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
+                <span>Privacy policy</span>
+              </li>
+            </Link>
+            <Link href="/policies/shipping">
+              <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
+                <span>Shipping policy</span>
+              </li>
+            </Link>
+            <Link href="/policies/payment">
+              <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
+                <span>Payment Policy</span>
+              </li>
+            </Link>
+            <Link href="/policies/repairing">
+              <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
+                <span>Repairing policy</span>
+              </li>
+            </Link>
+            <Link href="/policies/return">
+              <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
+                <span>Return policy</span>
+              </li>
+            </Link>
+            <Link href="/policies/cancellation">
+              <li className="border border-black text-[#000] rounded-[10px] text-[25px] px-5 py-3 cursor-pointer">
+                <span>Cancellation policy</span>
+              </li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </>
   );
