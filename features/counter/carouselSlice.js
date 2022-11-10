b4b6e9fec01b59b0a61264635d156210ee9ecb4d
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-11-10 20:29:34 +0500

    Updated

--- features/counter/carouselSlice.js
+++ features/counter/carouselSlice.js
@@ -1,15 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { carouselProductsData } from "../../components/common/carouselProductsData";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { carouselProductsData } from "../../components/common/carouselProductsData";
 
-const initialState = { Products: carouselProductsData };
+// const initialState = { Products: carouselProductsData };
 
-export const carouselProductsSlice = createSlice({
-  name: "carouselProducts",
-  initialState,
-  reducers: { showData(state, action) {} },
-});
+// export const carouselProductsSlice = createSlice({
+//   name: "carouselProducts",
+//   initialState,
+//   reducers: { showData(state, action) {} },
+// });
 
-// Action creators are generated for each case reducer function
-export const { showData } = carouselProductsSlice.actions;
+// // Action creators are generated for each case reducer function
+// export const { showData } = carouselProductsSlice.actions;
 
-export default carouselProductsSlice.reducer;
+// export default carouselProductsSlice.reducer;
